234b19d97cf93f4615b987e01de1270773efd33c
Matthew <mmartinezmateo93@gmail.com>
2017-09-19 12:05:05 +0200

    REV20170919 chapter 16 completed

---
+++ app/src/main/java/com/matthew/weatherapp/ui/App.kt
@@ -0,0 +1,28 @@
+package com.matthew.weatherapp.ui
+
+import android.app.Application
+import android.database.sqlite.SQLiteOpenHelper
+
+/**
+ * Created by Matthew on 19/09/2017.
+ */
+class App : Application() {
+    companion object {
+        var instance: App by DelegatesExt.notNullSingleValue()
+        //        private var instance: Application? = null;
+//        fun instance() = instance!!;
+
+    }
+
+    object DelegatesExt {
+        fun <T> notNullSingleValue() = NotNullSingleValueVar<T>();
+    }
+
+    override fun onCreate() {
+        super.onCreate();
+        instance = this;
+
+    }
+
+
+}
---
+++ app/src/main/java/com/matthew/weatherapp/ui/NotNullSingleValueVar.kt
@@ -0,0 +1,22 @@
+package com.matthew.weatherapp.ui
+
+import kotlin.reflect.KProperty
+
+/**
+ * Created by Matthew on 19/09/2017.
+ */
+class NotNullSingleValueVar<T> {
+    private var value: T? = null;
+
+
+
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        return value ?:
+                throw IllegalStateException("${property.name}" + "not initialized")
+    }
+
+    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        this.value = if (this.value == null) value else
+            throw IllegalStateException("${property.name} already initialized");
+    }
+}
